3c37f8693aa00c342bafb4159b46f62b3fc8243c
zhenhanmo <zhenhanmo@duocai100.com>
2017-01-23 10:06:21 +0800

    mavenModules/

---
+++ mavenModules/utils/src/main/java/com/maven/utils/web/TestController.java
@@ -0,0 +1,10 @@
+package com.maven.utils.web;
+
+/**
+ * Created by zhenhanmo on 2016/12/27.
+ */
+public class TestController {
+    public void funtion(){
+        System.out.println("大家好呀");
+    }
+}
